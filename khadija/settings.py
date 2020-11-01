e535fcb0e46c6ed49b64b520b0e5b78b5ad3ff6d
Your Name <you@example.com>
2020-11-01 12:14:07 +0100

    Added djnagi

--- khadija/settings.py
+++ khadija/settings.py
@@ -90,7 +90,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'download.apps.DownloadConfig',
+    'download',
 ]
 
 MIDDLEWARE = [
