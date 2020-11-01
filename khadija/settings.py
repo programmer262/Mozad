f69ac559957405ea102221012e764bb5c915bd3c
Your Name <you@example.com>
2020-11-01 12:30:48 +0100

    Added djnagi

--- khadija/settings.py
+++ khadija/settings.py
@@ -30,8 +30,8 @@ ALLOWED_HOSTS = ['khadioumelmouminine.herokuapp.com','127.0.0.1']
 
 
 INSTALLED_APPS = [
-    'jazzmin'
-    'download.apps.DownloadConfig',
+   
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'download.apps.DownloadConfig',
-    'download',
 ]
 
 MIDDLEWARE = [
