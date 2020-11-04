8a4c8439b62b14dd9291d46d0dfb75e8c8cbb672
programmer262 <65510490+programmer262@users.noreply.github.com>
2020-11-04 02:57:21 +0100

    Update settings.py

--- khadija/settings.py
+++ khadija/settings.py
@@ -186,5 +186,5 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = '/docs/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/docs')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/docs')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
