16f61d27d6370d2107f5e935f58cdc62503e6a0f
programmer262 <65510490+programmer262@users.noreply.github.com>
2020-11-10 19:19:53 +0100

    Update models.py

--- download/models.py
+++ download/models.py
@@ -102,7 +102,7 @@ class Live_ended (models.Model):
     matiére = models.ForeignKey(Matiére, on_delete=models.SET_NULL, null=True, blank=True)
     cour = models.CharField(max_length=100)
     partie = models.CharField(max_length=100)
-    live = models.CloudinaryField()
+    live = models.FileField()
 
 
     def __str__(self):
