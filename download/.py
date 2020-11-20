f1678b31f62b85e7b9e212f7ffc5aee91fc059f4
programmer262 <65510490+programmer262@users.noreply.github.com>
2020-11-20 12:30:26 +0100

    Update and rename views.py to .py

--- download/.py
+++ download/.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
-
 from django.contrib.auth import authenticate, login, logout
 
 from django.contrib import messages
@@ -13,17 +12,14 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 from .models import *
 
-@login_required
 def cour(request):
     cours = Cour.objects.all()
     context = {'cours':cours}
     return render(request, 'docfiles.html', context)
-@login_required
 def exercice(request):
     exercices = Exercice.objects.all()
     context = {'exercices':exercices}
     return render(request, 'exercices.html', context)
-@login_required
 def correction(request):
     corrigés = Corrigé.objects.all()
     context = {'corrigés':corrigés}
@@ -67,12 +63,11 @@ def loginPage(request):
 def logoutUser(request):
 	logout(request)
 	return redirect('login')
-@login_required
+
 def classe(request):
     classes = Classe.objects.all()
     context = {'classes':classes}
     return render(request, 'Live.html', context)
-@login_required
 def live(request):
     lives = Live_ended.objects.all()
     context = {'lives':lives}
