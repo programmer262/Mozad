f33315ad8cac39c2552e71d26fc10e9e55f3d900
programmer262 <65510490+programmer262@users.noreply.github.com>
2020-12-19 23:41:20 +0100

    Update admin.py

--- download/admin.py
+++ download/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.site_header = "2BACSPC-F-A"
-admin.site.site_title = "2BACSPC-F-A"
 admin.site.register(Cour)
+admin.site.register(Matiére)
+admin.site.register(Professor)
+admin.site.register(Etudiant)
 admin.site.register(Exercice)
 admin.site.register(Corrigé)
 admin.site.register(Classe)
