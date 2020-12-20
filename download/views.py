96afba3e105b0b4d84341d726787c3e92b78c666
programmer262 <65510490+programmer262@users.noreply.github.com>
2020-12-20 01:36:49 +0100

    Update views.py

--- download/views.py
+++ download/views.py
@@ -11,36 +11,23 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from .models import *
-
+@login_required
 def cour(request):
     cours = Cour.objects.all()
     context = {'cours':cours}
     return render(request, 'docfiles.html', context)
+@login_required
 def exercice(request):
     exercices = Exercice.objects.all()
     context = {'exercices':exercices}
     return render(request, 'exercices.html', context)
+@login_required
 def correction(request):
     corrigés = Corrigé.objects.all()
     context = {'corrigés':corrigés}
     return render(request, 'correction.html', context)
   
-def registerPage(request):
-	if request.user.is_authenticated:
-		return redirect('cour')
-	else:
-		form = CreateUserForm()
-		if request.method == 'POST':
-			form = CreateUserForm(request.POST)
-			if form.is_valid():
-				form.save()
-				user = form.cleaned_data.get('username')
-				messages.success(request, 'Account was created for ' + user)
 
-				return redirect('login')
-
-		context = {'form':form}
-		return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
 	if request.user.is_authenticated:
@@ -64,11 +51,18 @@ def logoutUser(request):
 	logout(request)
 	return redirect('login')
 
+@login_required
 def classe(request):
     classes = Classe.objects.all()
     context = {'classes':classes}
     return render(request, 'Live.html', context)
+@login_required
 def live(request):
     lives = Live_ended.objects.all()
     context = {'lives':lives}
     return render(request, 'endlives.html', context)
+@login_required
+def error(request):
+    cours = Cour.objects.all()
+    context = {'cours':cours}
+    return render(request, 'courview.html', context)
