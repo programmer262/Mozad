e6756193394f6eb154eda7c497618ec707778262
programmer262 <65510490+programmer262@users.noreply.github.com>
2021-01-01 14:34:06 +0100

    Update urls.py

--- download/urls.py
+++ download/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -6,10 +6,9 @@ urlpatterns = [
     path('',views.cour,name='cour'),
     path('exercice/',views.exercice,name='exercice'),
     path('correction/',views.correction,name='correction'),
-
 	path('accounts/login/', views.loginPage, name="login"),  
 	path('accounts/logout/', views.logoutUser, name="logout"),
 	path('classe/', views.classe, name="classe"),
 	path('endlive/', views.live, name="live"),
-
-	]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	path('error/', views.error, name="error"),
+    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
