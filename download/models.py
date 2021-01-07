dcc6ac285df39ba3bb345f2d7683c91c737b7fae
programmer262 <65510490+programmer262@users.noreply.github.com>
2021-01-07 17:29:01 +0100

    Update models.py

--- download/models.py
+++ download/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+  
+class Etudiant(models.Model): 
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
 class Matiére(models.Model):
   matiére = models.CharField(max_length=2000)
 
@@ -15,16 +21,7 @@ class Professor(models.Model):
 
   def __str__(self):
         return self.first_name + ' ' +self.last_name
-  
-class Etudiant(models.Model): 
-  user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-  first_name = models.CharField(max_length=200)
-  last_name = models.CharField(max_length=200)
-  email = models.CharField(max_length=2000)
-  téléphone = models.CharField(max_length=16)
 
-  def __str__(self):
-        return self.first_name + ' ' +self.last_name
   
     
 class Cour(models.Model):
