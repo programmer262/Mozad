aeeb21bb5db40fd2c7f07f344b4c6427858f2322
programmer262 <65510490+programmer262@users.noreply.github.com>
2021-01-09 15:25:18 +0100

    Update models.py

--- download/models.py
+++ download/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-  
-class Etudiant(models.Model): 
-    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200)
-    def __str__(self):
-        return self.name  
+
+class Matiére(models.Model):
+  matiére = models.CharField(max_length=2000)
+
+  def __str__(self):
+        return self.matiére
 class Professor(models.Model): 
   user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
   first_name = models.CharField(max_length=200)
@@ -17,17 +16,17 @@ class Professor(models.Model):
 
   def __str__(self):
         return self.first_name + ' ' +self.last _name
-class Matiére(models.Model):
-  matiére = models.CharField(max_length=2000)
-
-  def __str__(self):
-        return self.matiére
-
-
+  
+class Etudiant(models.Model): 
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name  
   
     
 class Cour(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    Professor = models.ForeignKey(Professor, on_delete=models.SET_NULL, null=True, blank=True)
     document =models.FileField()
     cour = models.CharField(max_length=200)
     partie = models.CharField(max_length=200,null=False,blank=False)
@@ -43,7 +42,7 @@ class Cour(models.Model):
            return url
 
 class Exercice(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    Professor = models.ForeignKey(Professor, on_delete=models.SET_NULL, null=True, blank=True)
     document = models.FileField()
     exercice = models.CharField(max_length=200)
     def __str__(self):
@@ -58,7 +57,7 @@ class Exercice(models.Model):
            return url
 
 class Corrigé(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    Professor = models.ForeignKey(Professor, on_delete=models.SET_NULL, null=True, blank=True)
     document = models.FileField()
     correction_name = models.CharField(max_length=200)
     def __str__(self):
@@ -75,7 +74,7 @@ class Corrigé(models.Model):
 
 
 class Classe(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    Professor = models.ForeignKey(Professor, on_delete=models.SET_NULL, null=True, blank=True)
     lien = models.CharField(max_length=2000)
     type_of_language =  models.CharField(max_length=2000)
     date =  models.CharField(max_length=200)
@@ -84,7 +83,7 @@ class Classe(models.Model):
     def __str__(self):
         return self.heure
 class Live_ended (models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    Professor = models.ForeignKey(Professor, on_delete=models.SET_NULL, null=True, blank=True)
     matiére = models.CharField(max_length=2000)
     cour = models.CharField(max_length=100)
     partie = models.CharField(max_length=100)
