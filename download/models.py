0cf132c743799f29c200ce0f86496885cd80125b
programmer262 <65510490+programmer262@users.noreply.github.com>
2021-01-09 15:24:14 +0100

    Update models.py

--- download/models.py
+++ download/models.py
@@ -8,11 +8,15 @@ class Etudiant(models.Model):
     def __str__(self):
         return self.name  
 class Professor(models.Model): 
-    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200)
-    def __str__(self):
-        return self.name
+  user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+  first_name = models.CharField(max_length=200)
+  last_name = models.CharField(max_length=200)
+  email = models.CharField(max_length=2000)
+  téléphone = models.CharField(max_length=16)
+  Matiére = models.ForeignKey(Matiére, on_delete=models.SET_NULL, null=True, blank=True)
+
+  def __str__(self):
+        return self.first_name + ' ' +self.last _name
 class Matiére(models.Model):
   matiére = models.CharField(max_length=2000)
 
