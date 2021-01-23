537a0081f8361699f5a7ba1be763dcaef70b1f10
programmer262 <65510490+programmer262@users.noreply.github.com>
2021-01-23 16:05:46 +0100

    Update admin.py

--- download/admin.py
+++ download/admin.py
@@ -3,9 +3,11 @@ from .models import *
 
 admin.site.register(Cour)
 admin.site.register(Matiére)
-admin.site.register(Professor)
 admin.site.register(Etudiant)
 admin.site.register(Exercice)
 admin.site.register(Corrigé)
 admin.site.register(Classe)
 admin.site.register(Live_ended)
+@admin.register(Professeur)
+class ProfesseurAdmin(admin.ModelAdmin):
+     list_display = ['user','telephone','email','matiére']
